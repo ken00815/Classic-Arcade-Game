0732d595de0ee5d68178e7e5516a6b5fa43ad2c5
Ken <KenandChristie@Christie-and-Kens-MacBook-Pro.local>
2018-02-05 22:52:31 +0800

    placed first enemy and new player onto the screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
+    var obj = Object.create(Enemy.prototype);
+    this.x = x;
+    this.y = y;
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +18,9 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x < 606){
+    this.x += dt * 101;
+  } else {}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,7 +31,10 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function() {
+var Player = function(x,y) {
+  var obj = Object.create(Player.prototype);
+  this.x = x;
+  this.y = y;
   this.sprite = 'images/char-boy.png';
 };
 
@@ -33,26 +43,21 @@ Player.prototype.update = function() {
 };
 
 Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
 
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
-var player = {
-  "update": function() {
+var enemy1 = new Enemy(0,60+83);
 
-  },
-  "render": function() {
+var allEnemies = [enemy1];
 
-  },
-  "handleInput": function() {
-    
-  }
-};
-
-
+var player = new Player(202,83*4+60);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
